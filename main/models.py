747af7e451f011b8e96ab3ddcee3442f033159c2
JaguarKovalev <jaguarkovalev@gmail.com>
2025-02-17 13:23:33 +0300

    add clients fuctionality

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    email = models.EmailField(max_length=50, blank=False, null=False)
+    phone = models.CharField(max_length=20, blank=False, null=False)
+    description = models.TextField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
--- main/views.py
+++ main/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ClientForm  
 
 def index(request):
-    return render(request, 'main/index.html')
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return redirect('index') 
+    else:
+        form = ClientForm()
+
+    return render(request, 'main/index.html', {'form': form})
