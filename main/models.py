49a93a64a19dc49b36557d88a4838fbf896c5475
JaguarKovalev <jaguarkovalev@gmail.com>
2025-02-17 13:25:40 +0300

    change descs of fields of model

--- main/models.py
+++ main/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=100, blank=False, null=False)
-    email = models.EmailField(max_length=50, blank=False, null=False)
-    phone = models.CharField(max_length=20, blank=False, null=False)
-    description = models.TextField(max_length=500, blank=True, null=True)
+    name = models.CharField("Имя", max_length=100, blank=False, null=False)
+    email = models.EmailField("Почта", max_length=50, blank=False, null=False)
+    phone = models.CharField("Номер телефона", max_length=20, blank=False, null=False)
+    description = models.TextField("Запрос", max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.name
